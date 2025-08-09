dfe2afc7fdf2cfebc4d739a3b522d8dddb033881
Aman Sheikh <haarishsheikh04@gmail.com>
2025-08-09 09:42:03 +0530

    Implement contact us page

--- app/(app)/contact-us/page.tsx
+++ app/(app)/contact-us/page.tsx
@@ -1,222 +1,138 @@
-
-"use client"; // Add this to ensure it's a Client Component
-
-import React from "react";
-import { Button, Input, Textarea, Typography } from "@material-tailwind/react";
 import Image from "next/image";
+import React from "react";
 
-export function ContactSection14() {
+const page = () => {
   return (
     <div className="bg-white">
-    <section className="px-8 py-8 lg:py-16">
-      <div className="container mx-auto text-center">
-        <Typography
-          variant="h5"
-          color="blue-gray"
-          className="mb-4 !text-base lg:!text-2xl"
-          placeholder={undefined}
-          onResize={undefined}
-          onResizeCapture={undefined}
-          onPointerEnterCapture={undefined}
-          onPointerLeaveCapture={undefined}
-        >
-          Customer Care
-        </Typography>
-        <Typography
-          variant="h1"
-          color="blue-gray"
-          className="mb-4 !text-3xl lg:!text-5xl"
-          placeholder={undefined}
-          onResize={undefined}
-          onResizeCapture={undefined}
-          onPointerEnterCapture={undefined}
-          onPointerLeaveCapture={undefined}
-        >
-          We&apos;re Here to Help
-        </Typography>
-        <Typography
-          className="mb-10 font-normal !text-lg lg:mb-20 mx-auto max-w-3xl !text-gray-500"
-          placeholder={undefined}
-          onResize={undefined}
-          onResizeCapture={undefined}
-          onPointerEnterCapture={undefined}
-          onPointerLeaveCapture={undefined}
-        >
-          Whether it&apos;s a question about our services, a request for
-          technical assistance, or suggestions for improvement, our team is
-          eager to hear from you.
-        </Typography>
-        <div className="grid grid-cols-1 gap-x-12 gap-y-6 lg:grid-cols-2 items-start">
-          <Image
-            height={500}
-            width={500}
-            src="/image/map.svg"
-            alt="map"
-            className="w-full h-full lg:max-h-[510px]"
-          />
-          <form action="#" className="flex flex-col gap-4 lg:max-w-sm">
-            <Typography
-              variant="small"
-              className="text-left !font-semibold !text-gray-600"
-              placeholder={undefined}
-              onResize={undefined}
-              onResizeCapture={undefined}
-              onPointerEnterCapture={undefined}
-              onPointerLeaveCapture={undefined}
-            >
-              Select Options for Business Engagement
-            </Typography>
-            <div className="flex gap-4">
-              <Button
-                variant="outlined"
-                className="max-w-fit"
-                placeholder={undefined}
-                onResize={undefined}
-                onResizeCapture={undefined}
-                onPointerEnterCapture={undefined}
-                onPointerLeaveCapture={undefined}
-              >
-                General inquiry
-              </Button>
-              <Button
-                variant="outlined"
-                className="max-w-fit"
-                placeholder={undefined}
-                onResize={undefined}
-                onResizeCapture={undefined}
-                onPointerEnterCapture={undefined}
-                onPointerLeaveCapture={undefined}
-              >
-                Product Support
-              </Button>
-            </div>
-            <div className="grid grid-cols-2 gap-4">
-              <div>
-                <Typography
-                  variant="small"
-                  className="mb-2 text-left font-medium !text-gray-900"
-                  placeholder={undefined}
-                  onResize={undefined}
-                  onResizeCapture={undefined}
-                  onPointerEnterCapture={undefined}
-                  onPointerLeaveCapture={undefined}
-                >
-                  First Name
-                </Typography>
-                <Input
-                  color="gray"
-                  size="lg"
-                  placeholder="First Name"
-                  name="first-name"
-                  className="focus:border-t-gray-900"
-                  containerProps={{
-                    className: "min-w-full",
-                  }}
-                  labelProps={{
-                    className: "hidden",
-                  }}
-                />
-              </div>
-              <div>
-                <Typography
-                  variant="small"
-                  className="mb-2 text-left font-medium !text-gray-900"
-                  placeholder={undefined}
-                  onResize={undefined}
-                  onResizeCapture={undefined}
-                  onPointerEnterCapture={undefined}
-                  onPointerLeaveCapture={undefined}
-                >
-                  Last Name
-                </Typography>
-                <Input
-                  color="gray"
-                  size="lg"
-                  placeholder="Last Name"
-                  name="last-name"
-                  className="focus:border-t-gray-900"
-                  containerProps={{
-                    className: "!min-w-full",
-                  }}
-                  labelProps={{
-                    className: "hidden",
-                  }}
-                />
-              </div>
-            </div>
+      {/* Contact */}
+      <div className="max-w-7xl px-4 sm:px-6 lg:px-8 py-12 lg:py-24 mx-auto ">
+        <div className="mb-6 sm:mb-10 max-w-2xl text-center mx-auto">
+          <h2 className="font-medium text-black text-2xl sm:text-4xl">
+            Contacts
+          </h2>
+        </div>
+
+        <div className="grid grid-cols-1 lg:grid-cols-2 lg:items-center gap-6 md:gap-8 lg:gap-12">
+          <div className="aspect-w-16 aspect-h-6 lg:aspect-h-14 overflow-hidden bg-gray-100 rounded-2xl">
+            <Image
+              height={500}
+              width={600}
+              className="group-hover:scale-105 group-focus:scale-105 transition-transform duration-500 ease-in-out object-cover rounded-2xl"
+              src="https://images.unsplash.com/photo-1534536281715-e28d76689b4d?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGNvbnRhY3R8ZW58MHx8MHx8fDA%3D"
+              alt="Contacts Image"
+            />
+          </div>
+          {/* End Col */}
+
+          <div className="space-y-8 lg:space-y-16">
             <div>
-              <Typography
-                variant="small"
-                className="mb-2 text-left font-medium !text-gray-900"
-                placeholder={undefined}
-                onResize={undefined}
-                onResizeCapture={undefined}
-                onPointerEnterCapture={undefined}
-                onPointerLeaveCapture={undefined}
-              >
-                Your Email
-              </Typography>
-              <Input
-                color="gray"
-                size="lg"
-                placeholder="name@email.com"
-                name="email"
-                className="focus:border-t-gray-900"
-                containerProps={{
-                  className: "!min-w-full",
-                }}
-                labelProps={{
-                  className: "hidden",
-                }}
-              />
+              <h3 className="mb-5 font-semibold text-black">Our address</h3>
+
+              {/* Grid */}
+              <div className="grid sm:grid-cols-2 gap-4 sm:gap-6 md:gap-8 lg:gap-12">
+                <div className="flex gap-4">
+                  <svg
+                    className="shrink-0 size-5 text-gray-500"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="1.5"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  >
+                    <path d="M20 10c0 6-8 12-8 12s-8-6-8-12a8 8 0 0 1 16 0Z"></path>
+                    <circle cx="12" cy="10" r="3"></circle>
+                  </svg>
+
+                  <div className="grow">
+                    <p className="text-sm text-gray-600">United Kingdom</p>
+                    <address className="mt-1 text-black not-italic">
+                      300 Bath Street, Tay House
+                      <br />
+                      Glasgow G2 4JR
+                    </address>
+                  </div>
+                </div>
+              </div>
+              {/* End Grid */}
             </div>
+
             <div>
-              <Typography
-                variant="small"
-                className="mb-2 text-left font-medium !text-gray-900"
-                placeholder={undefined}
-                onResize={undefined}
-                onResizeCapture={undefined}
-                onPointerEnterCapture={undefined}
-                onPointerLeaveCapture={undefined}
-              >
-                Your Message
-              </Typography>
-              <Textarea
-                rows={6}
-                color="gray"
-                placeholder="Message"
-                name="message"
-                className="focus:border-t-gray-900"
-                containerProps={{
-                  className: "!min-w-full",
-                }}
-                labelProps={{
-                  className: "hidden",
-                }}
-                onResize={undefined}
-                onResizeCapture={undefined}
-                onPointerEnterCapture={undefined}
-                onPointerLeaveCapture={undefined}
-              />
+              <h3 className="mb-5 font-semibold text-black">Our contacts</h3>
+
+              {/* Grid */}
+              <div className="grid sm:grid-cols-2 gap-4 sm:gap-6 md:gap-8 lg:gap-12">
+                <div className="flex gap-4">
+                  <svg
+                    className="shrink-0 size-5 text-gray-500"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="1.5"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  >
+                    <path d="M21.2 8.4c.5.38.8.97.8 1.6v10a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V10a2 2 0 0 1 .8-1.6l8-6a2 2 0 0 1 2.4 0l8 6Z"></path>
+                    <path d="m22 10-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 10"></path>
+                  </svg>
+
+                  <div className="grow">
+                    <p className="text-sm text-gray-600">Email us</p>
+                    <p>
+                      <a
+                        className="relative inline-block font-medium text-black before:absolute before:bottom-0.5 before:start-0 before:-z-1 before:w-full before:h-1 before:bg-yellow-400 hover:before:bg-black focus:outline-hidden focus:before:bg-black"
+                        href="mailto:example@site.so"
+                      >
+                        hello@example.so
+                      </a>
+                    </p>
+                  </div>
+                </div>
+
+                <div className="flex gap-4">
+                  <svg
+                    className="shrink-0 size-5 text-gray-500"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  >
+                    <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z" />
+                  </svg>
+
+                  <div className="grow">
+                    <p className="text-sm text-gray-600">Call us</p>
+                    <p>
+                      <a
+                        className="relative inline-block font-medium text-black before:absolute before:bottom-0.5 before:start-0 before:-z-1 before:w-full before:h-1 before:bg-yellow-400 hover:before:bg-black focus:outline-hidden focus:before:bg-black"
+                        href="mailto:example@site.so"
+                      >
+                        +44 222 777-000
+                      </a>
+                    </p>
+                  </div>
+                </div>
+              </div>
+              {/* End Grid */}
             </div>
-            <Button
-              className="w-full"
-              color="gray"
-              placeholder={undefined}
-              onResize={undefined}
-              onResizeCapture={undefined}
-              onPointerEnterCapture={undefined}
-              onPointerLeaveCapture={undefined}
-            >
-              Send message
-            </Button>
-          </form>
+          </div>
+          {/* End Col */}
         </div>
       </div>
-    </section>
+      {/* End Contact */}
     </div>
   );
-}
+};
 
-export default ContactSection14;
+export default page;
--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["tailwindcss.com", "images.unsplash.com", "plus.unsplash.com", "upload.wikimedia.org", "media.istockphoto.com"],
+    domains: ["tailwindcss.com", "images.unsplash.com", "plus.unsplash.com", "upload.wikimedia.org", "media.istockphoto.com", "www.material-tailwind.com"],
   },
 };
 
